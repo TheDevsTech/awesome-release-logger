0abc28b3b174de11236d4cf2bb75aa01db4164c6
H.R. Shadhin <dev@hrshadhin.me>
2020-01-29 14:15:59 +0600

    feat: 2nd commit

--- main.go
+++ main.go
@@ -5,27 +5,51 @@ import (
     "fmt"
     "log"
     "os/exec"
+    "strings"
+    "os"
 )
 
 const ShellToUse = "bash"
 
-func Shellout(command string) (error, string, string) {
+func Shellout(command string) (string) {
     var stdout bytes.Buffer
     var stderr bytes.Buffer
     cmd := exec.Command(ShellToUse, "-c", command)
     cmd.Stdout = &stdout
     cmd.Stderr = &stderr
     err := cmd.Run()
-    return err, stdout.String(), stderr.String()
+
+    if err != nil {
+        log.Printf("error: %v\n", err)
+        fmt.Println("--- Error Trace ---")
+        fmt.Println(stderr.String())
+        os.Exit(1)
+    }
+
+    return stdout.String()
 }
 
 func main() {
-    err, out, errout := Shellout("ls -ltr")
-    if err != nil {
-        log.Printf("error: %v\n", err)
+    gitLogCommand := "git log --format=%B%H----DELIMITER----"
+    out := Shellout(gitLogCommand)
+    commitsArray := strings.Split(out, "----DELIMITER----\n")
+    commitDict := make(map[string]string)
+    for _, commit := range commitsArray {
+        commitPart := strings.Split(commit, "\n")
+        if len(commitPart) == 2 {
+            commitDict[string(commitPart[1])] = string(commitPart[0])
+        }
     }
-    fmt.Println("--- stdout ---")
-    fmt.Println(out)
-    fmt.Println("--- stderr ---")
-    fmt.Println(errout)
+
+    for sha, message := range commitDict {
+        fmt.Println(sha, message)
+    }
+
+
+
+
+
+
+    //fmt.Println("--- stdout ---")
+    //fmt.Println(out)
 }
